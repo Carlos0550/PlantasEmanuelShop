9700cf49d9212d851a2a1a9eeeeedcde40b227f6
carlos0550 <carlospelinski03@gmail.com>
2024-12-08 03:33:02 -0300

    Gestiona todo lo relacionado a productos

---
+++ src/Admin/pages/ProductManager/ProductsManager.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProductsManager() {
+  return (
+    <React.Fragment>
+        <h2>Sección de productos</h2>
+        <h3>Administra aqui tu lista de stock</h3>
+    </React.Fragment>
+  )
+}
+
+export default ProductsManager
