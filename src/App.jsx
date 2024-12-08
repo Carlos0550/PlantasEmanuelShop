b9825503e0d050e43c4fb92cf77fbc13a8bd41c2
carlos0550 <carlospelinski03@gmail.com>
2024-12-08 03:32:10 -0300

    Añade rutas para dashboard y productos

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router-dom"
 import Login from "./Admin/pages/Auth/Login"
 import "./App.css"
 import Register from "./Admin/pages/Auth/Register"
+import Dashboard from "./Admin/pages/Dashboard/Dashboard"
+import LayoutComponent from "./Components/Layout/Layout"
+import ProductsManager from "./Admin/pages/ProductManager/ProductsManager"
 function App() {
   return (
     <Routes>
       {/* Rutas de administrador */}
       <Route path="/" element={<Login />}/>
       <Route path="/register" element={<Register />}/>
-
+      <Route path="/dashboard" element={<LayoutComponent component={<Dashboard/>}/>}/>
+      <Route path="/manage-products" element={<LayoutComponent component={<ProductsManager/>}/>}/>
 
       {/* Rutas para los clientes */}
     </Routes>
