d7449333b9e921556bdcb8ecc69875a98d91f95c
carlos0550 <carlospelinski03@gmail.com>
2024-12-08 23:54:29 -0300

    Añade categorias

---
+++ src/Admin/pages/ProductManager/Forms/AddCategories.jsx
@@ -0,0 +1,43 @@
+import { Button, Form, Input } from 'antd'
+import React, { useState } from 'react'
+import { useAppContext } from '../../../../AppContext';
+
+function AddCategories() {
+    const [form] = Form.useForm();
+    const { saveCategory, getCategories } = useAppContext()
+    const [uploading, setUploading] = useState(false)
+    const onFinish = async(values) => {
+        setUploading(true)
+        const result = await saveCategory(values.category_name)
+        setUploading(false)
+        if(result){
+            getCategories()
+            form.resetFields()
+        }
+    }
+  return (
+    <React.Fragment>
+        <Form
+            form={form}
+            onFinish={onFinish}
+            // layout='vertical'
+            autoComplete="off"
+            style={{ width: "100%" }}
+        >
+            <Form.Item
+                label="Categoría"
+                name="category_name"
+                rules={[{ required: true, message: 'Por favor ingrese una categoría' }]}
+            >
+                <Input placeholder='Ingrese el nombre de la categoría'/>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type='primary' htmlType='submit' loading={uploading}>Guardar</Button>
+            </Form.Item>
+        </Form>
+    </React.Fragment>
+  )
+}
+
+export default AddCategories
