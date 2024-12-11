04fffa7f7ac1fbb0e5105ee026b97a306d9dd65a
carlos0550 <carlospelinski03@gmail.com>
2024-12-11 17:27:25 -0300

    Implementa edicion de productos

--- src/Admin/pages/ProductManager/Forms/AddProducts.jsx
+++ src/Admin/pages/ProductManager/Forms/AddProducts.jsx
@@ -5,15 +5,32 @@ import { useAppContext } from '../../../../AppContext';
 import { v4 as uuidv4 } from 'uuid';
 import { resizeAndConvertImages } from '../../../../utils/ResizeImages';
 
+import { EditorState, ContentState, convertToRaw, convertFromHTML } from 'draft-js';
+import { stateToHTML } from 'draft-js-export-html';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
 const { Dragger } = Upload;
 
 function AddProducts() {
     const [form] = Form.useForm();
-    const { categories, saveProduct, getProducts } = useAppContext();
-    const [fileList, setFileList] = useState([]);
+    const { categories, saveProduct, getProducts, editingProduct, 
+        productId, handleProducts, productsList, editProducts
 
-    const uploadProps = {
-        name: 'file',
+    } = useAppContext();
+    console.log(productId)
+    const [fileList, setFileList] = useState([]);
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+    const onEditorStateChange = (newState) => {
+        setEditorState(newState);
+        const rawContext = convertToRaw(newState.getCurrentContent());
+        const plainText = rawContext.blocks.map(block => block.text).join('\n');
+        form.setFieldsValue({
+            product_description: plainText,
+        });
+    };
+    const [uploadProps, setUploadProps] = useState({name: 'file',
         multiple: true,
         beforeUpload: async (file) => {
             try {
@@ -22,7 +39,8 @@ function AddProducts() {
                     uid: uuidv4(),
                     name: compressedFile.name,
                     status: 'done',
-                    originFileObj: compressedFile, 
+                    originFileObj: compressedFile,
+                    thumbUrl: URL.createObjectURL(compressedFile) 
                 };
 
                 setFileList((prevList) => [...prevList, newFileList]);
@@ -35,30 +53,83 @@ function AddProducts() {
         },
         fileList,
         onRemove: (file) => {
-            setFileList((prevList) => prevList.filter((item) => item.uid !== file.uid));
-        },
-    };
-
+            setFileList((prevList) => prevList.filter((item) => item.name.split(".")[0] !== file.name.split(".")[0]));
+        },});
+    
+        useEffect(()=>{
+            setUploadProps((prevProps) => ({
+                ...prevProps,
+                fileList, 
+            }));
+        },[fileList])
+        
     const onFinish = async (values) => {
+        console.log(values)
+        const htmlDescription = stateToHTML(editorState.getCurrentContent());
+
         const formData = new FormData();
         for (const key in values) {
-            if(key !== "product_images"){
+            if(key !== "product_images" && key !== "product_description") {
                 formData.append(key, values[key] || "");
             }
         }
-
+        formData.append('product_description', htmlDescription);
         fileList.forEach((file) => {
             formData.append('images', file.originFileObj);
         });
 
-        const result = await saveProduct(formData)
+        const result = editingProduct ? await editProducts(formData, productId) : await saveProduct(formData)
         if(result){
-            message.success('Producto registrado con exito')
+            if(editingProduct) message.success('Producto editado con exito') 
+            else message.success('Producto registrado con exito')
+
             form.resetFields()
             setFileList([])
+            setEditorState(EditorState.createEmpty())
             getProducts()
+            handleProducts()
         }
     };
+
+    useEffect(()=>{
+        if(editingProduct && productId){
+            const selectedProduct = productsList.find(product => product.id === productId)
+            console.log(selectedProduct)
+            form.setFieldsValue({
+                product_name: selectedProduct.product_name,
+                product_category: selectedProduct.product_category,
+                product_price: selectedProduct.product_price,
+                product_description: selectedProduct.product_description,
+                
+            })
+            const blockFromHTML = convertFromHTML(selectedProduct.product_description);
+            const contentState = ContentState.createFromBlockArray(blockFromHTML.contentBlocks, blockFromHTML.entityMap);
+            const editorState = EditorState.createWithContent(contentState);
+            setEditorState(editorState)
+
+            const formattedImages = selectedProduct.images.map((image) => ({
+                uid: uuidv4(),
+                name: image.image_name,
+                status: 'done',
+                originFileObj: new File([image.image_data], image.image_name, { type: image.image_type }),
+                
+            }))
+    
+            setFileList(formattedImages)
+            
+        }else{
+            form.resetFields()
+            setFileList([])
+            setEditorState(EditorState.createEmpty())
+
+        }
+    },[editingProduct, productId])
+
+useEffect(()=>{
+    form.setFieldsValue({
+        product_images: fileList
+    })
+},[fileList])
 
     return (
         <Form
@@ -107,7 +178,15 @@ function AddProducts() {
                 label="Descripción"
                 rules={[{ required: true, message: 'Por favor ingrese la descripción' }]}
             >
-                <Input.TextArea placeholder='Ingrese la descripción' />
+                <Editor
+                    editorState={editorState}
+                    onEditorStateChange={onEditorStateChange}
+                    placeholder='Ingrese la descripción del producto'
+                    
+                    toolbar={{
+                        options: ['inline', 'list', 'textAlign', 'link', 'history'],
+                    }}
+                />
             </Form.Item>
 
             <Form.Item
@@ -115,9 +194,10 @@ function AddProducts() {
                 label="Imágenes"
                 rules={[{ required: true, message: 'Por favor suba al menos una imagen' }]}
                 valuePropName="fileList"
+               
                 getValueFromEvent={(e) => e && e.fileList}
             >
-                <Dragger {...uploadProps}>
+                <Dragger {...uploadProps} fileList={fileList}>
                     <p className="ant-upload-drag-icon">
                         <InboxOutlined />
                     </p>
