61813dcf1839e750db378b06cd023a06ab76f8ff
carlos0550 <carlospelinski03@gmail.com>
2024-12-12 18:59:19 -0300

    ajustes visuales

--- src/Admin/pages/ProductManager/ProductsManager.jsx
+++ src/Admin/pages/ProductManager/ProductsManager.jsx
@@ -4,33 +4,34 @@ import AddCategories from './Forms/AddCategories'
 import AddProducts from './Forms/AddProducts'
 import CategoriesTable from './Tables/CategoriesTable'
 import ProductsTable from './Tables/ProductsTable'
+import { useAppContext } from '../../../AppContext'
+import Title from 'antd/es/typography/Title'
 function ProductsManager() {
+    const { editingProduct } = useAppContext()
+
   return (
     <React.Fragment>
-      <h2>Sección de productos</h2>
-      <h3>Administra aqui tu lista de stock</h3>
+      <Title>
+        Sección de productos
+      </Title>
+      <Title level={3}>Administra aqui tu lista de stock</Title>
       <Row gutter={[16, 16]}>
             <Col xl={12} lg={24}>
-                <Card title="Agregar nuevo producto">
+                <Card title="Agregar nuevo producto" style={{opacity: editingProduct ? 0.4 : 1}}>
                     <AddProducts/>
                 </Card>
             </Col>
 
-            <Col xl={12} lg={24}>
+            <Col xl={12} lg={24} xs={24}>
                 <Card title="Agregar nueva categoría">
                     <AddCategories />
+                    <CategoriesTable/>
                 </Card>
             </Col>
 
             <Col xs={24}>
                 <Card title="Listado de productos">
                     <ProductsTable/>
-                </Card>
-            </Col>
-
-            <Col xs={24}>
-                <Card title="Listado de categorías">
-                    <CategoriesTable/>
                 </Card>
             </Col>
         </Row>
