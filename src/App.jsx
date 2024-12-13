665875a712f21340dbf95fc67a9a8d5850c9d691
carlos0550 <carlospelinski03@gmail.com>
2024-12-13 17:54:29 -0300

    Agrega ruta para administrar clientes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./Admin/pages/Auth/Register"
 import Dashboard from "./Admin/pages/Dashboard/Dashboard"
 import LayoutComponent from "./Components/Layout/Layout"
 import ProductsManager from "./Admin/pages/ProductManager/ProductsManager"
+import ClientsManager from "./Admin/pages/Clients/ClientsManager"
 function App() {
   return (
     <Routes>
@@ -13,6 +14,7 @@ function App() {
       <Route path="/register" element={<Register />}/>
       <Route path="/dashboard" element={<LayoutComponent component={<Dashboard/>}/>}/>
       <Route path="/manage-products" element={<LayoutComponent component={<ProductsManager/>}/>}/>
+      <Route path="/manage-categories" element={<LayoutComponent component={<ClientsManager/>}/>}/>
 
       {/* Rutas para los clientes */}
     </Routes>
