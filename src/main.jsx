5a7bc6953ecd0acddcb1ca064b2f1b6e4c5cc94a
carlos0550 <carlospelinski03@gmail.com>
2024-12-16 19:25:40 -0300

    Agrega un config provider para el idioma en español

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './AppContext.jsx'
+import esES from 'antd/es/locale/es_ES';
+import { ConfigProvider } from 'antd'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <AppProvider>
-    <App />
-    </AppProvider>
+      <ConfigProvider locale={esES}>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </ConfigProvider>
     </BrowserRouter>
   </StrictMode>,
 )
